140e5ac95f593093fd7a34d3719127dbed0ab975
Krisztian Czako <krisztian@linuxakademia.com>
2022-06-28 13:52:25 +0100

    Changed default DOCROOT to /var/www (Apache default), create it if not exists.

--- apachestart.sh
+++ apachestart.sh
@@ -3,11 +3,10 @@
 : "${SERVERNAME:=unconfigured-apache-server}"
 : "${SERVERADMIN:=admin@example.com}"
 : "${HTTPPORT:=80}"
-: "${DOCROOT:=/var/www/html}"
+: "${DOCROOT:=/var/www}"
 
-if [ ! -d $DOCROOT ] ; then
-    echo "The Document root DOCROOT=$DOCROOT doesn't exists."
-    exit 1
+if [ ! -d ${DOCROOT} ] ; then
+    mkdir -p ${DOCROOT}
 fi
 
 mkdir -p /run/apache2
@@ -15,27 +14,27 @@ chown root.root /etc/ssl/apache2/*
 chmod 600 /etc/ssl/apache2/*
 
 cat <<EOF > /etc/apache2/conf.d/01main.conf
-Listen $HTTPPORT
-ServerAdmin $SERVERADMIN
-ServerName $SERVERNAME:$HTTPPORT
-DocumentRoot "$DOCROOT"
-<Directory "$DOCROOT">
+Listen ${HTTPPORT}
+ServerAdmin ${SERVERADMIN}
+ServerName ${SERVERNAME}:${HTTPPORT}
+DocumentRoot "${DOCROOT}"
+<Directory "${DOCROOT}">
     Options Indexes FollowSymLinks
     AllowOverride All
     Require all granted
 </Directory>
 EOF
 
-if [ x$HTTPSPORT != "x" ] ; then
+if [ x${HTTPSPORT} != "x" ] ; then
     /usr/local/bin/genhttps.sh
 fi
 
-if [ x$FCGI != "x" ] ; then
+if [ x${FCGI} != "x" ] ; then
 cat <<EOF > /etc/apache2/conf.d/fcgi.conf
 LoadModule proxy_module /usr/lib/apache2/mod_proxy.so
 LoadModule proxy_fcgi_module /usr/lib/apache2/mod_proxy_fcgi.so
 <IfModule mod_proxy_fcgi.c>
-        ProxyPassMatch "^/(.*\\.php(/.*)?)$" "fcgi://$FCGI$DOCROOT/\$1"
+        ProxyPassMatch "^/(.*\\.php(/.*)?)$" "fcgi://${FCGI}${DOCROOT}/\$1"
 	ProxyTimeout 600
         DirectoryIndex /index.php index.php
 	ProxyVia Block
